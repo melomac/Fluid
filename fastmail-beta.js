27fa4a839e26f9f2370d97634e464d04ed89cd53
noar <noar@domain.tld>
2012-11-12 02:25:12 +0100

    added Growl support for FastMail.FM Beta webmail

--- fastmail-beta.js
+++ fastmail-beta.js
@@ -7,23 +7,23 @@ URL pattern: *fastmail.fm/mail/*
 
 */
 
-window.fluid.dockBadge = "";
 setTimeout(updateDockBadge, 3000);
 setInterval(updateDockBadge, 10000);
 
 INBOX_ONLY = false;
 FILTERED = [ "Drafts", "Trash", "Spam" ];
 
-function updateDockBadge() {
+function updateDockBadge()
+{
     var count = 0;
-    
+
     var tree = document.getElementsByClassName("FolderTree")[0];
     for (i = 0;  i < tree.childNodes.length; i++)
     {
         name = tree.childNodes[i].getElementsByClassName("name")[0].innerText;
         badge = tree.childNodes[i].getElementsByClassName("badge")[0].innerText;
         // console.log("name: " + name + ", badge: " + badge);
-        
+
         if (badge)
         {
             if (! INBOX_ONLY || name == "Inbox")
@@ -37,9 +37,21 @@ function updateDockBadge() {
             }
         }
     }
-    
-    // console.log("count: " + count);
-    if (count != 0) {
+
+    // console.log("new count: " + count ", current count: " + window.fluid.dockBadge);
+    if (count > window.fluid.dockBadge)
+    {
+        notification = {
+            title: "FastMail.FM Beta",
+            description: "You have " + count + " unread message(s)",
+            priority: 1,
+            sticky: false,
+            identifier: "fastmail-beta",
+            icon: window.fluid.resourcePath + 'app.icns'
+        };
+        window.fluid.showGrowlNotification(notification);
+        window.fluid.dockBadge = count;
+    } else if (count > 0) {
         window.fluid.dockBadge = count;
     } else {
         window.fluid.dockBadge = "";
